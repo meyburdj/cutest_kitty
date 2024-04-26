30415513ed9771d1c3b73621939abe8ea43c03d3
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-04-26 09:28:01 -0400

    adding CatCard and fetching logic

---
+++ app/components/CatCard.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+interface CatCardProps {
+    url: string;
+    score: number;
+}
+
+const CatCard: React.FC<CatCardProps> = ({ url, score }) => {
+    return (
+        <div className="flex flex-col items-center p-4 shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-300">
+            <Image
+                src={url}
+                width={250}
+                height={250}
+                alt="Picture of a cat"
+                className="rounded-lg"
+            />
+            <p className="mt-2 text-lg font-semibold">score: {score}</p>
+        </div>
+    );
+};
+
+export default CatCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-export default function Home() {
+import CatCard from '@/app/components/CatCard'
+
+export default async function Home() {
+
+  const res = await fetch(`${process.env.BACKEND_API_URL}/cats`)
+  const data = await res.json()
+  console.log(data.groups[0].cats)
+  let cats = data.groups[0].cats
+
   return (
     <div className="flex flex-col items-center justify-start min-h-screen py-2 pt-12">
       <button className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Kitty Time!
       </button>
+      <div className="flex flex-row flex-wrap justify-center gap-4 mt-8">
+        {cats.map((cat, index) => (
+          <CatCard key={index} url={cat.url} score={cat.score} />
+        ))}
+      </div>
     </div>
   );
 }
+
