ff033e61ea210cd5582b7a3f2dd9ed6761541f44
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-04-26 22:19:51 -0400

    changed caching policy

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomeClient from './PageClient';
 
 export default async function Home() {
 
-  const res = await fetch(`${process.env.BACKEND_API_URL}/cats`)
+  const res = await fetch(`${process.env.BACKEND_API_URL}/cats`, { next: { revalidate: 0 } })
   const data = await res.json()
   console.log(data.groups[0].cats)
   let groups = data.groups
